2a852b4a7ea723be1fc655b2fad8b76cb0ee92fc
Skyler.Ab <85718273+SklyerX@users.noreply.github.com>
2023-08-30 15:11:28 -0400

    banner msg changed

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className="bg-[#18181c] fixed top-0 left-0 w-full h-8 mb-[10rem] grid place-items-center z-[99]">
       <span className="text-white font-['Inter'] font-bold uppercase">
-        Stop the war in iran
+        Free Iran
       </span>
     </div>
   );
